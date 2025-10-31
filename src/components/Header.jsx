e0c1f152ff8fbde4d4d45e14169ba4f0cd47bfde
Retr0981 <davidarmah991@gmail.com>
2025-10-31 15:42:50 +0000

    added theme changer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import Link from 'next/link'
-
-
-function Header() {
+import Link from 'next/link';
+import DarkModeSwitch from './DarkModeSwitch';
+export default function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
-       <ul className="flex gap-4">
-        <li>
-            <Link href={'/sign'}>Sign In</Link>
-        </li>
-       <li className='hidden sm:block'>
-            <Link href={'/'}>Home</Link>
+      <ul className='flex gap-4'>
+        <li className='hidden sm:block'>
+          <Link href={'/'}>Home</Link>
         </li>
         <li className='hidden sm:block'>
-            <Link href={'/about'}>About</Link>
+          <Link href={'/favorites'}>Favorites</Link>
         </li>
-       </ul>
-       <Link href={'/'} className="flex gap-1 items-center">
-       <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
-        IMDB 
-       </span>
-       <span className="text-xl hidden sm:inline">Clone </span>
-       </Link>
+        <li className='hidden sm:block'>
+          <Link href={'/about'}>About</Link>
+        </li>
+      </ul>
+      <div className='flex items-center gap-4'>
+        <DarkModeSwitch />
+        <Link href={'/'} className='flex gap-1 items-center'>
+          <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>
+            IMDb
+          </span>
+          <span className='text-xl hidden sm:inline'>Clone</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Header
